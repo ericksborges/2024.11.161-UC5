d5052f6f7e84abf258ef0a3487a75268acad4eee
Valtemir Procopio <i3831@rn.senac.local>
2025-07-03 20:40:02 -0300

    docs: criado a documentação com as convenções do projeto
     - Criado o diretorio mvc
    
     - Criado o diretorio docs

--- Aula 02/index.js
+++ Aula 02/index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORTA;
 
 // parsear dados em formato json
-app.use(express.json())
+app.use(express.json());
 const produtos = [];
 
 // rota para listar todos produtos
@@ -19,7 +19,7 @@ app.get("/produtos", (requisicao, resposta) => {
   } catch (error) {
     resposta.status(500).json({
       mensagem: "Erro interno do servidor. Por favor tente mais tarde!",
-      erro: error.message
+      erro: error.message,
     });
   }
 });
@@ -28,12 +28,53 @@ app.get("/produtos", (requisicao, resposta) => {
 app.post("/produtos/cadastrar", (requisicao, resposta) => {
   try {
     const { id, nome, preco, descricao } = requisicao.body;
-    if(!id || !nome || !preco || !descricao){
-        return resposta.status(400).json({mensagem: 'Todos os campos são obrigatorios!'})
+    if (!id || !nome || !preco || !descricao  ) {
+      return resposta
+        .status(400)
+        .json({ mensagem: "Todos os campos são obrigatorios!" });
     }
     const novoProduto = { id, nome, preco, descricao };
     produtos.push(novoProduto);
     resposta.status(201).json({ mensagem: "Produto criado com sucesso!" });
+  } catch (error) {
+    resposta.status(500).json({
+      mensagem: "Erro interno do servidor. Por favor tente mais tarde!",
+      erro: error.message,
+    });
+  }
+});
+
+// atualizando um produto existente
+app.patch("/produtos/atualizar/:id", (requisicao, resposta) => {
+  try {
+    const { novoNome, novoPreco, novaDescricao } = requisicao.body;
+    // 1
+    const id = parseInt(requisicao.params.id);
+    const produto = produtos.find((produto) => produto.id === id);
+    if (!produto) {
+      return resposta.status(404).json({ mensagem: "Produto não encontrado!" });
+    }
+    produto.nome = novoNome || produto.nome;
+    produto.preco = novoPreco || produto.preco;
+    produto.descricao = novaDescricao || produto.descricao;
+    resposta.status(200).json({ mensagem: "Produto atualizado com sucesso" });
+  } catch (error) {
+    resposta.status(500).json({
+      mensagem: "Erro interno do servidor. Por favor tente mais tarde!",
+      erro: error.message,
+    });
+  }
+});
+
+// listar um produto pelo id
+app.get("/produtos/listar/:id", (requisicao, resposta) => {
+  try {
+    const id = parseInt(requisicao.params.id);
+    const produto = produtos.find((produto) => produto.id === id);
+    if (!produto) {
+      return resposta.status(404).json({ mensagem: "Produto não encontrado!" });
+    }
+    resposta.status(200).json(produto);
   } catch (error) {
     resposta.status(500).json({
       mensagem: "Erro interno do servidor. Por favor tente mais tarde!",
@@ -42,35 +83,38 @@ app.post("/produtos/cadastrar", (requisicao, resposta) => {
   }
 });
 
-// atualizando um produto existente
-app.patch("/produtos/atualizar/:id", (requisicao, resposta) => {
-    try {
-        const { novoNome, novoPreco, novaDescricao} = requisicao.body
-        const id = parseInt(requisicao.params.id)
-        const produto = produtos.find(produto => produto.id === id)
-        if(!produto){
-            return resposta.status(404).json({mensagem: 'Produto não encontrado!'})
-        }
-        produto.nome = novoNome || produto.nome
-        produto.preco = novoPreco || produto.preco
-        produto.descricao = novaDescricao || produto.descricao
-        resposta.status(200).json({mensagem: 'Produto atualizado com sucesso'})
-    } catch (error) {
-        resposta.status(500).json({
-            mensagem: "Erro interno do servidor. Por favor tente mais tarde!",
-            erro: error.message
-          });
-    }
+// deletar todos os produtos
+app.delete("/produtos/deletar", (requisicao, resposta) => {
+  try {
+    produtos.length = 0;
+    resposta
+      .status(200)
+      .json({ mensagem: "Todos os produtos foram deletados!" });
+  } catch (error) {
+    resposta.status(500).json({
+      mensagem: "Erro interno do servidor. Por favor tente mais tarde!",
+      erro: error.message,
+    });
+  }
 });
 
-// listar um produto pelo id
-app.get("/produtos/listar/:id", (requisicao, resposta) => {});
-
-// deletar todos os produtos
-app.delete("/produtos/deletar", (requisicao, resposta) => {});
-
 // deletar produto por id
-app.delete("/produtos/deletar/:id", (requisicao, resposta) => {});
+app.delete("/produtos/deletar/:id", (requisicao, resposta) => {
+  try {
+    const id = parseInt(requisicao.params.id)
+    const index = produtos.findIndex(produto => produto.id === id)
+    if (index === -1) {
+      return resposta.status(404).json({ mensagem: "Produto não encontrado!" });
+    }
+    produtos.splice(index, 1)
+    resposta.status(200).json({mensagem: 'Produto excluido com sucesso!'})
+  } catch (error) {
+    resposta.status(500).json({
+      mensagem: "Erro interno do servidor. Por favor tente mais tarde!",
+      erro: error.message,
+    });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
